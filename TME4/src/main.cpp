a4276c2eec0b036a72a4a278319e216af44e9d43
num <numcaps@outlook.fr>
2022-10-17 06:38:15 +0200

    error thread

--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -3,27 +3,30 @@
 using namespace std;
 
 const int NB_THREAD = 10;
-size_t SOLDEINITIAL = 50;
+int SOLDEINITIAL = 50;
 size_t K = 10; /* Nombre de comptes*/
 
-void Q1(pr::Banque &b, size_t i, size_t j, size_t m)
+void Q1(pr::Banque &b)
 {
-	b.transfert(i, j, m);
-	std::this_thread::sleep_for (std::chrono::milliseconds(20));
-
+	size_t i = rand() % 99 + 1;
+	size_t j = rand() % 99 + 1;
+	size_t m = rand() % 99 + 1;
+	for (size_t c = 0; c < 1000; c++)
+	{
+		b.transfert(i, j, m);
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
 }
 int main()
 {
 	/* Creation de la banque */
 	pr::Banque b(K, SOLDEINITIAL);
-	size_t i = rand() % 99 + 1;
-	size_t j = rand() % 99 + 1;
-	size_t m = rand() % 99 + 1;
+
 	vector<thread> threads;
 	// TODO : creer des threads qui font ce qui est demandé
-	for (size_t c = 0; c < 1000; c++)
+	for (size_t c = 0; c < NB_THREAD; c++)
 	{
-		threads.emplace_back(Q1, std::ref(b), i, j, m);
+		threads.emplace_back(Q1, std::ref(b));
 	}
 
 	for (auto &t : threads)
