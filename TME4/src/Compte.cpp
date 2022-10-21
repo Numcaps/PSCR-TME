3169e70b1e90291e06de33668ab64af8fabf67aa
num <numcaps@outlook.fr>
2022-10-21 10:47:30 +0200

    tme4 non fini

--- TME4/src/Compte.cpp
+++ TME4/src/Compte.cpp
@@ -2,28 +2,45 @@
 
 using namespace std;
 
-namespace pr {
-void Compte::crediter (unsigned int val) {
-	unique_lock<mutex> g(m);
-	solde+=val ;
-}
-bool Compte::debiter (unsigned int val) {
-	unique_lock<mutex> g(m);
-	bool doit = solde >= val;
-	if (doit) {
-		solde-=val ;
+namespace pr
+{
+	void Compte::crediter(unsigned int val)
+	{
+		unique_lock<recursive_mutex> g(m);
+		solde += val;
 	}
-	return doit;
-}
-int Compte::getSolde() const  {
-	unique_lock<mutex> g(m);
-	return solde;
-}
-// NB : vector exige Copyable, mais mutex ne l'est pas...
-Compte::Compte(const Compte & other) {
-	other.m.lock();
-	solde = other.solde;
-	other.m.unlock();
-}
-
+	bool Compte::debiter(unsigned int val)
+	{
+		unique_lock<recursive_mutex> g(m);
+		bool doit = solde >= val;
+		if (doit)
+		{
+			solde -= val;
+		}
+		return doit;
+	}
+	int Compte::getSolde() const
+	{
+		unique_lock<recursive_mutex> g(m);
+		return solde;
+	}
+	// NB : vector exige Copyable, mais mutex ne l'est pas...
+	Compte::Compte(const Compte &other)
+	{
+		other.m.lock();
+		solde = other.solde;
+		other.m.unlock();
+	}
+	void Compte::lock() const
+	{
+		m.lock();
+	}
+	void Compte::unlock() const
+	{
+		m.unlock();
+	}
+	bool Compte::try_lock() const
+	{
+		return m.try_lock();
+	}
 }
--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -1,34 +1,37 @@
 #include "Banque.h"
-
+#include <iostream>
 using namespace std;
 
-const int NB_THREAD = 10;
+const int NB_THREAD = 100;
 int SOLDEINITIAL = 50;
-size_t K = 10; /* Nombre de comptes*/
+size_t K = 2; /* Nombre de comptes*/
 
 void Q1(pr::Banque &b)
 {
-	size_t i = rand() % 99 + 1;
-	size_t j = rand() % 99 + 1;
-	size_t m = rand() % 99 + 1;
-	for (size_t c = 0; c < 1000; c++)
+	size_t i = rand() % K; /* ATTENTION INDICE DOIT CORRESPONDRE AU NOMBRE DE COMPTE */
+	size_t j = rand() % K;
+	size_t m = rand() % K + 1;
+	for (size_t c = 0; c < 10000; c++)
 	{
 		b.transfert(i, j, m);
-		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+		std::cout << "Transfert de " << m << " $, de "<<i<<" a "<< j<<std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 20 + 1));
 	}
+
 }
+
 int main()
 {
 	/* Creation de la banque */
 	pr::Banque b(K, SOLDEINITIAL);
 
 	vector<thread> threads;
-	// TODO : creer des threads qui font ce qui est demandé
+	//TODO : creer des threads qui font ce qui est demandé
 	for (size_t c = 0; c < NB_THREAD; c++)
 	{
 		threads.emplace_back(Q1, std::ref(b));
 	}
-
+	
 	for (auto &t : threads)
 	{
 		t.join();
